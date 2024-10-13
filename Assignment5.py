20909711dd7925de8ab4c96c2d3def55065d0810
Edwin Miyatake <122956602+edmiyatake@users.noreply.github.com>
2024-10-13 14:28:20 -0700

    attempt to start assignment 5

---
+++ Assignment5.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class heapBuilder:
+    def __init__(self):
+        self.root = None
+    
+    def minHeap(self,arr):
+        exit
+
+    def maxHeap(self,arr):
+        exit
